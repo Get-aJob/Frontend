e6e4ac8676b99e9a613e1d5af253b7f6b2457039
leekyuhyun <leekh010502@gmail.com>
2026-03-15 23:07:28 +0900

    ✨ Feat : 레이아웃 테스트를 위한 각 페이지별 뷰 컴포넌트 초안 구현 #10

---
+++ src/view/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>통계 분석 페이지입니다.</h1>;
+}
+
+export default Dashboard;
---
+++ src/view/Scrap.tsx
@@ -0,0 +1,5 @@
+function Scrap() {
+  return <h1>좋아요한 공고 리스트 페이지입니다.</h1>;
+}
+
+export default Scrap;
