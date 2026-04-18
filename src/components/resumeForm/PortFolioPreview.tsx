c1ef8c5b6c425d695acadb9bb74d34ff215cbdbb
맥북 프로16 <a2485@maegbug-ui-MacBookPro.local>
2026-04-18 22:14:48 +0900

    🛠️ Fix : pdf preview z-index 수정

--- src/components/resumeForm/PortFolioPreview.tsx
+++ src/components/resumeForm/PortFolioPreview.tsx
@@ -32,7 +32,7 @@ const PortFolioPreview = ({ name }: PortFolioPreviewProps) => {
   if (!isOpen || !previewUrl) return null;
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm z-30">
+    <div className="fixed inset-0 flex justify-center items-center top-0 left-0 w-full h-full bg-slate-900/40  backdrop-blur-sm z-40">
       <div
         ref={previewRef}
         onClick={(e) => e.stopPropagation()}
